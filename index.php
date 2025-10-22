6cecb859fdbaf8eb25bf8c20c389673a3cb1d29a
Alex <86182397+alehxs@users.noreply.github.com>
2025-10-22 10:38:45 -0500

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  // These are 2 diff variables
+  $language = "php";
+  $Language = "PHP";
+  
+  echo $language;
+  echo $Language;
+  ?>
+</body>
+</html>
